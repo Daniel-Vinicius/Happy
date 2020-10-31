7ef851339cf54904a1a7d2132b2d90059aad154c
Daniel-Vinicius <dev.dan.programador@gmail.com>
2020-10-31 13:49:46 -0300

    Animação no Motion

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import "../styles/pages/landing.css";
 
 import logoImg from "../images/logo.svg";
+import markerImg from '../images/map-marker.svg';
 
 function Landing() {
   return (
     <div id='page-landing'>
-      <div className='content-wrapper'>
-        <img src={logoImg} alt='Happy' />
+
+<motion.div
+        className="wrapper-logo-happy"
+        initial={{ scale: 0 }}
+        animate={{ scale: 2 }}
+        transition={{ duration: 1.5, repeat: 1, repeatType: "reverse" }}
+      >
+        <img src={markerImg} alt="Logo happy" />
+      </motion.div>
+
+      <motion.div
+        className="content-wrapper"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 3, duration: 0.8 }}
+      >
+
+      <img src={logoImg} alt="Logo da plataforma Happy" />
 
         <main>
           <h1>Leve felicidade para o mundo</h1>
@@ -25,7 +43,8 @@ function Landing() {
         <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
         </Link>
-      </div>
+        </motion.div>
+  
     </div>
   );
 }
